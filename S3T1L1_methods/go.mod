3470bf0a8ca7a9de29eb4ba2b278ff813749f97b
dima_bo <dim.bod@gmail.com>
2022-08-20 20:13:22 +0300

    home

---
+++ S3T1L1_methods/go.mod
@@ -0,0 +1,3 @@
+module github.com/dimsonson/YPracticum_Go1/S3T1L1_methods
+
+go 1.19
--- S3T1L1_methods/main.go
+++ S3T1L1_methods/main.go
@@ -1,11 +1,76 @@
 package main
 
 import (
-	"time"
 	"fmt"
+	"time"
 )
 
-import
+type Stopwatch struct {
+	StartTime time.Time
+	Split     []time.Duration
+}
+
+func main() {
+	sw := Stopwatch{}
+	sw.Start()
+
+	time.Sleep(1 * time.Second)
+	sw.SaveSplit()
+
+	time.Sleep(500 * time.Millisecond)
+	sw.SaveSplit()
+
+	time.Sleep(300 * time.Millisecond)
+	sw.SaveSplit()
+
+	fmt.Println(sw.GetResults())
+	//fmt.Println(sw)
+}
+
+// запустить/сбросить секундомер
+func (stw *Stopwatch) Start() {
+	stw.StartTime = time.Now()
+	stw.Split = nil
+}
+
+// сохранить промежуточное время
+func (stw *Stopwatch) SaveSplit() {
+	stw.Split = append(stw.Split, time.Since(stw.StartTime))
+}
+
+// вернуть текущие результаты
+func (stw *Stopwatch) GetResults() []time.Duration {
+	return stw.Split
+}
+
+/* package main
+
+import (
+    "fmt"
+    "time"
+)
+
+type Stopwatch struct {
+    startTime time.Time
+    splits    []time.Time
+}
+
+func (sw *Stopwatch) Start() {
+    sw.startTime = time.Now()
+    sw.splits = nil
+}
+
+func (sw *Stopwatch) SaveSplit() {
+    sw.splits = append(sw.splits, time.Now())
+}
+
+func (sw Stopwatch) GetResults() (retResults []time.Duration) {
+    for _, splitTime := range sw.splits {
+        retResults = append(retResults, splitTime.Sub(sw.startTime))
+    }
+
+    return
+}
 
 func main() {
     sw := Stopwatch{}
@@ -21,4 +86,4 @@ func main() {
     sw.SaveSplit()
 
     fmt.Println(sw.GetResults())
-} 
+}  */
