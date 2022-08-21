e2a7a8b896f4ea722fb74700a32c43c11314a07d
dima_bo <dim.bod@gmail.com>
2022-08-21 23:29:28 +0300

    hom

--- S3T2L1_interface_concept/go.mod
+++ S3T2L1_interface_concept/go.mod
@@ -1,3 +1,8 @@
 module main
 
 go 1.19
+
+require (
+	github.com/dimsonson/YPracticum_Go1/S3T2L1_interface_concept/company v0.0.0-20220821200703-9ed4f5ec3370
+	github.com/dimsonson/YPracticum_Go1/S3T2L1_interface_concept/person v0.0.0-20220821200703-9ed4f5ec3370
+)
--- S3T2L1_interface_concept/main.go
+++ S3T2L1_interface_concept/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-    "company"
-    "person"
+	"fmt"
+
+	"github.com/dimsonson/YPracticum_Go1/S3T2L1_interface_concept/company"
+	"github.com/dimsonson/YPracticum_Go1/S3T2L1_interface_concept/person"
+	//"company"
+	//	"person"
 )
 
 func main() {
-    pers := person.Person{}
-    comp := company.Company{}
-    
-	// мы передаём переменную типа Person в функцию, 
-	//аргументом которой является переменная Worker! 
-    comp.Hire(pers) 
+	pers := person.Person{}
+	comp := company.Company{}
 
+	// мы передаём переменную типа Person в функцию,
+	//аргументом которой является переменная Worker!
+	comp.Hire(pers)
+	fmt.Println(pers)
+	fmt.Println(comp)
+	
 }
- 
