905b961a0b7ba93876a7346261e93486596f8b9b
dima_bo <dim.bod@gmail.com>
2022-08-22 17:45:10 +0300

    offf

--- S3T2L2_interface_std_lib/exampleW_1/main.go
+++ S3T2L2_interface_std_lib/exampleW_1/main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/dimsonson/YPracticum_Go1/S3T2L2_interface_std_lib/example/randbyte"
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"strings"
 )
 
-func main() {
-
-	// создаём генератор случайных чисел
-	generator := randbyte.New(time.Now().UnixNano()) // в качестве затравки передаём ему
-	//текущее время, и при каждом запуске оно будет разным.
-
-	buf := make([]byte, 16)
-
-	for i := 0; i < 5; i++ {
-		n, _ := generator.Read(buf) // единственный доступный метод, но он нам и нужен.
-		fmt.Printf("Generate bytes: %v size(%d)\n", buf, n)
+func LimitReader(r io.Reader, n int) io.Reader {
+	buf := make([]byte, n)
+	r1 := bytes.NewReader(buf)
+	_, err := r.Read(buf)
+	if err != nil {
+		log.Fatal(err)
 	}
+	return r1
+}
 
+func main() {
+	r := strings.NewReader("some io.Reader stream to be read\n")
+	lr := LimitReader(r, 4)
+
+	_, err := io.Copy(os.Stdout, lr)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
