a09aa856a584874f282ce3d74b3a93461f716b49
dima_bo <dim.bod@gmail.com>
2022-08-22 13:08:32 +0300

    offi

--- S3T2L2_interface_std_lib/main.go
+++ S3T2L2_interface_std_lib/main.go
@@ -1,57 +1,21 @@
 package main
 
 import (
-	"errors"
+	"example/randbyte"
 	"fmt"
-	"io"
-	"strings"
+	"time"
 )
 
 func main() {
 
-	s := `Hodor. Hodor hodor, hodor. Hodor hodor hodor hodor hodor. Hodor. Hodor! 
-Hodor hodor, hodor; hodor hodor hodor. Hodor. Hodor hodor; hodor hodor - hodor, 
-hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor; hodor hodor; 
-hodor hodor hodor! Hodor hodor HODOR! Hodor hodor... Hodor hodor hodor...`
+	// создаём генератор случайных чисел
+	generator := randbyte.New(time.Now().UnixNano()) // в качестве затравки передаём ему текущее время, и при каждом запуске оно будет разным.
 
-	// обернём строчку в strings.Reader
-	r := strings.NewReader(s)
+	buf := make([]byte, 16)
 
-	// создадим буфер на 16 байт
-	b := make([]byte, 16)
-
-	for {
-		// strings.Reader скопирует 16 байт в b
-		//
-		// в структуре запоминается последний указатель,
-		// то есть следующий вызов скопирует следующую порцию из 16 байт
-		//
-		// также метод возвращает количество прочитанных байт n и ошибку err
-		//
-		// когда дойдём до конца строки, метод отдаст ошибку io.EOF
-		n, err := r.Read(b)
-
-		// при работе с интерфейсом io.Reader нужно в первую очередь проверять
-		// n > 0, затем err != nil
-		//
-		// могут быть ситуации, когда часть данных получилось прочитать
-		// и сохранить в буфер, а затем произошла ошибка
-		//
-		// в таком случае будут одновременно n > 0 и err != nil
-		if n > 0 {
-			// выведем на экран содержимое буфера
-			fmt.Printf("%s\n", b)
-		}
-
-		if err != nil {
-			// если дочитали до конца, выходим из цикла
-			if errors.Is(err, io.EOF) {
-				break
-			}
-
-			// обрабатываем ошибку чтения
-			fmt.Printf("error: %v\n", err)
-		}
+	for i := 0; i < 5; i++ {
+		n, _ := generator.Read(buf) // единственный доступный метод, но он нам и нужен.
+		fmt.Printf("Generate bytes: %v size(%d)\n", buf, n)
 	}
 
 }
